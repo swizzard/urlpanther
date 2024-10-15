7085e153aa6c899cde53455340a9485e1fe09654
Sam Raker <sam.raker@gmail.com>
2024-10-15 14:34:45 -0400

    tests pass

--- src/segment.test.ts
+++ src/segment.test.ts
@@ -1,5 +1,5 @@
 import * as segment from "./segment";
-import visitNode from "./visit";
+import visitNodes from "./visit";
 import { describe, expect, it } from "@jest/globals";
 
 describe("one path", () => {
@@ -9,7 +9,7 @@ describe("one path", () => {
         const node = new segment.TerminalStringNode();
         const input = ["foo"];
         const expected = [{ matchedSegment: "foo", stringValue: "foo" }];
-        const actual = visitNode(node, input);
+        const actual = visitNodes([node], input);
         expect(actual).toEqual(expected);
       });
       describe("failure", () => {
@@ -17,14 +17,14 @@ describe("one path", () => {
           const node = new segment.TerminalStringNode();
           const input: Array<string> = [];
           const expected: Array<segment.SegmentMatch> = [];
-          const actual = visitNode(node, input);
+          const actual = visitNodes([node], input);
           expect(actual).toEqual(expected);
         });
         it("too much", () => {
           const node = new segment.TerminalStringNode();
           const input: Array<string> = ["foo", "bar"];
           const expected: Array<segment.SegmentMatch> = [];
-          const actual = visitNode(node, input);
+          const actual = visitNodes([node], input);
           expect(actual).toEqual(expected);
         });
       });
@@ -34,14 +34,14 @@ describe("one path", () => {
         const node = new segment.TerminalStaticNode("foo");
         const input = ["foo"];
         const expected = [{ matchedSegment: "foo", stringValue: "foo" }];
-        const actual = visitNode(node, input);
+        const actual = visitNodes([node], input);
         expect(actual).toEqual(expected);
       });
       it("failure", () => {
         const node = new segment.TerminalStaticNode("foo");
         const input = ["bar"];
         const expected: Array<segment.SegmentMatch> = [];
-        const actual = visitNode(node, input);
+        const actual = visitNodes([node], input);
         expect(actual).toEqual(expected);
       });
     });
@@ -50,14 +50,14 @@ describe("one path", () => {
         const node = new segment.TerminalNumberNode();
         const input = ["42"];
         const expected = [{ matchedSegment: "42", numberValue: 42 }];
-        const actual = visitNode(node, input);
+        const actual = visitNodes([node], input);
         expect(actual).toEqual(expected);
       });
       it("failure", () => {
         const node = new segment.TerminalNumberNode();
         const input = ["foo"];
         const expected: Array<segment.SegmentMatch> = [];
-        const actual = visitNode(node, input);
+        const actual = visitNodes([node], input);
         expect(actual).toEqual(expected);
       });
     });
@@ -68,14 +68,14 @@ describe("one path", () => {
         const expected = [
           { matchedSegment: "2021-01-01", dateValue: new Date("2021-01-01") },
         ];
-        const actual = visitNode(node, input);
+        const actual = visitNodes([node], input);
         expect(actual).toEqual(expected);
       });
       it("failure", () => {
         const node = new segment.TerminalDateNode();
         const input = ["2021-01-01-01-01"];
         const expected: Array<segment.SegmentMatch> = [];
-        const actual = visitNode(node, input);
+        const actual = visitNodes([node], input);
         expect(actual).toEqual(expected);
       });
     });
@@ -91,7 +91,7 @@ describe("one path", () => {
             { matchedSegment: "the", stringValue: "the" },
             { matchedSegment: "end", stringValue: "end" },
           ];
-          const actual = visitNode(ntn, input);
+          const actual = visitNodes([ntn], input);
           expect(actual).toEqual(expected);
         });
         it("StaticNode", () => {
@@ -102,7 +102,7 @@ describe("one path", () => {
             { matchedSegment: "the", stringValue: "the" },
             { matchedSegment: "end", stringValue: "end" },
           ];
-          const actual = visitNode(ntn, input);
+          const actual = visitNodes([ntn], input);
           expect(actual).toEqual(expected);
         });
       });
@@ -112,7 +112,7 @@ describe("one path", () => {
           const ntn = new segment.NonTerminalStringNode(undefined, [tn]);
           const input = ["the"];
           const expected: Array<segment.SegmentMatch> = [];
-          const actual = visitNode(ntn, input);
+          const actual = visitNodes([ntn], input);
           expect(actual).toEqual(expected);
         });
         it("too much", () => {
@@ -120,7 +120,7 @@ describe("one path", () => {
           const ntn = new segment.NonTerminalStringNode(undefined, [tn]);
           const input = ["the", "end", "and"];
           const expected: Array<segment.SegmentMatch> = [];
-          const actual = visitNode(ntn, input);
+          const actual = visitNodes([ntn], input);
           expect(actual).toEqual(expected);
         });
         it("bad match", () => {
@@ -128,7 +128,7 @@ describe("one path", () => {
           const ntn = new segment.NonTerminalStaticNode("the", undefined, [tn]);
           const input = ["the", "beginning"];
           const expected: Array<segment.SegmentMatch> = [];
-          const actual = visitNode(ntn, input);
+          const actual = visitNodes([ntn], input);
           expect(actual).toEqual(expected);
         });
       });
@@ -146,11 +146,46 @@ describe("choices", () => {
       ]);
       const input = ["the", "end"];
       const expected = [
-        { matchedSegment: "the", stringValue: "the" },
-        { matchedSegment: "end", stringValue: "end" },
+        { matchedSegment: "the", stringValue: "the", label: "non-terminal" },
+        { matchedSegment: "end", stringValue: "end", label: "right end" },
       ];
-      const actual = visitNode(ntn, input);
+      const actual = visitNodes([ntn], input);
       expect(actual).toEqual(expected);
+    });
+    describe("mix term/non-term", () => {
+      const term1 = new segment.TerminalStaticNode("profile", "term1");
+      const term2 = new segment.TerminalStringNode("userId");
+      const medial = new segment.NonTerminalStaticNode("profile", "medial", [
+        term2,
+      ]);
+      const start = new segment.NonTerminalStaticNode("user", "start", [
+        medial,
+        term1,
+      ]);
+      it("term1", () => {
+        const input = ["user", "profile"];
+        const expected = [
+          { matchedSegment: "user", stringValue: "user", label: "start" },
+          { matchedSegment: "profile", stringValue: "profile", label: "term1" },
+        ];
+        const actual = visitNodes([start], input);
+        expect(actual).toEqual(expected);
+      });
+      it("term2", () => {
+        const userId = "abc";
+        const input = ["user", "profile", userId];
+        const expected = [
+          { matchedSegment: "user", stringValue: "user", label: "start" },
+          {
+            matchedSegment: "profile",
+            stringValue: "profile",
+            label: "medial",
+          },
+          { matchedSegment: userId, stringValue: userId, label: "userId" },
+        ];
+        const actual = visitNodes([start], input);
+        expect(actual).toEqual(expected);
+      });
     });
   });
 });
--- src/visit.ts
+++ src/visit.ts
@@ -1,31 +1,31 @@
 import { SegmentNode, SegmentMatch } from "./segment";
 
-export default function visitNode(
-  node: SegmentNode,
+export default function visitNodes(
+  nodes: Array<SegmentNode>,
   segments: Array<string>,
   progress: Array<SegmentMatch> = [],
 ): Array<SegmentMatch> {
+  if (nodes.length === 0) {
+    return progress;
+  }
   // this shouldn't ever happen
   if (segments.length === 0) {
     console.error("segments is empty");
-    return progress;
-  }
-  if (node.label) {
-    console.log(`visiting ${node.label} with ${segments}`);
-  }
-  const [_segment, ...rest] = segments;
-  const res = node.accept(segments);
-  console.log(`got res: ${JSON.stringify(res)}`);
-  if (res === null) {
     return [];
-  } else {
-    const newProgress = [...progress, res];
-    for (const child of node.children) {
-      const childRes = visitNode(child, rest, newProgress);
-      if (childRes.length >= 0) {
-        return childRes;
-      }
-    }
-    return newProgress;
   }
+  for (const node of nodes) {
+    if (node.label) {
+      console.log(`visiting ${node.label} with ${segments}`);
+    }
+    const [_segment, ...rest] = segments;
+    const res = node.accept(segments);
+    console.log(`got res: ${JSON.stringify(res)}`);
+    if (res === null) {
+      continue;
+    } else {
+      const newProgress = [...progress, res];
+      return visitNodes(node.children, rest, newProgress);
+    }
+  }
+  return [];
 }
